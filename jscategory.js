d78266212bfb3bedcc7fc7845958febd48df1e54
Mike Stay <metaweta@gmail.com>
2016-05-14 01:56:12 -0700

    Some renaming, move examples out of main file

--- jscategory.js
+++ jscategory.js
@@ -6,9 +6,7 @@ var CT = (function (){
   var getClassName = call.bind({}.toString);
   var create = Object.create.bind(Object);
   var gpo = Object.getPrototypeOf.bind(Object);
-  var cache = new WeakMap();
-
-  // Makes the contracts into global methods
+ 
   var install = function (global) {
     for (var i in CT) {
       global[i] = CT[i];
@@ -46,7 +44,6 @@ var CT = (function (){
   var array = classOf("Array");
   var date = classOf("Date");
   var regexp = classOf("RegExp");
-  var object = classOf("Object");
 
   // Creates a contract for a value of type s
   var typeOf = function (s) {
@@ -60,6 +57,7 @@ var CT = (function (){
 
   var func = typeOf("function"); 
   var string = typeOf("string");
+  var object = typeOf("object");
   var boolean = typeOf("boolean");
   var number = typeOf("number");
 
@@ -94,7 +92,7 @@ var CT = (function (){
   // The mapClean function only passes the element.
   Object.defineProperty(Array.prototype, "mapClean", {
     writable: true, 
-    enumerable:false, 
+    enumerable: false, 
     configurable: true, 
     value: function (f) {
       return this.map(function (x) { return f(x); });
@@ -194,9 +192,9 @@ var CT = (function (){
     objectOf(func)(cs);
     return function (choice) {
       array(choice);
-      str(choice[0]);
+      string(choice[0]);
       if (choice.length !== 2) {
-        throw new TypeError("Expected [str, any].");
+        throw new TypeError("Expected [string, any].");
       }
       if (!cs.hasOwnProperty(choice[0])) {
         throw new TypeError("Unknown tag.")
@@ -305,12 +303,12 @@ var CT = (function (){
   // As a functor, creates a memoized version of a
   // function f:str -> any
   var memo = function (c) {
-    c = hom(str, any)(c);
+    c = hom(string, any)(c);
     var memo = {};
     return function (s) {
       // The "$" prefix avoids issues with
       // magic properties like __proto__.
-      var key = "$" + str(s);
+      var key = "$" + string(s);
       if (key in memo) {
         return memo[key];
       }
@@ -319,7 +317,9 @@ var CT = (function (){
   };
 
   var CT = {
+    allOf: intersect,
     any: any,
+    anyOf: union,
     array: array,
     arrayOf: arrayOf,
     boolean: boolean,
@@ -331,6 +331,7 @@ var CT = (function (){
     id: any,
     instanceOf: instanceOf,
     int32: int32,
+    intersect: intersect,
     memo: memo,
     nat32: nat32,
     number: number,
@@ -341,6 +342,7 @@ var CT = (function (){
     prods: prods,
     regexp: regexp,
     string: string,
+    union: union,
 
     install: install
   };
@@ -351,221 +353,3 @@ var CT = (function (){
 ///////////////////////////////////////////////////
 
 CT.install(this);
-
-// Algebraic theory of MONOIDS:
-// Choose a set and two functions satisfying some relations, get a monoid
-// It's undecidable to test the relations, so we leave them out...
-var monoid = function (set, times, ident) {
-  return prods({
-    t: func,
-    "*": hom(set, set, set),
-    1: set
-  })({
-    t: set,
-    "*": times,
-    1: ident
-  });
-};
-
-// ... and write tests instead.
-// Given a monoid and a way to get random elements of the monoid,
-// check associativity and unit laws.
-var testMonoid = function (mon, rand) {
-  var a = rand(), b = rand(), c = rand();
-  var i = mon[1];
-  var t = mon["*"];
-  if (t(a, t(b, c)) !== t(t(a, b), c)) {
-    throw new Error("Monoid is not associative: " + stringify([a, b, c]));
-  }
-  if (t(a, i) !== a) {
-    throw new Error("Monoid does not satisfy a * 1 = a: " + a);
-  }
-  if (t(i, a) !== a) {
-    throw new Error("Monoid does not satisfy 1 * a = a: " + a);
-  }
-};
-
-// Alternate definition of a monoid that helps illustrate
-// the concept of a monoid homomorphism
-var monoid2 = function (setIn, setOut, times, ident) {
-  // Allow the other signature as well
-  if (arguments.length === 3) {
-    ident = times;
-    times = setOut;
-    setOut = setIn;
-  }
-  return prods({
-    "*": hom(setIn, setIn, setOut),
-    1: hom(/*terminal,*/ setOut)
-  })({
-    "*": times,
-    1: ident
-  });
-};
-
-var K = function (x) { return function () { return x; }; };
-
-var and = hom(boolean, boolean, boolean)(function (b1, b2) { return b1 && b2; });
-var andMon = monoid2(boolean, and, K(true));
-
-var xor = hom(boolean, boolean, boolean)(function (b1, b2) { return (b1 ^ b2) ? true : false; });
-var xorMon = monoid2(boolean, xor, K(true));
-
-var int32Add = hom(int32, int32, int32)(function (n1, n2) { return n1 + n2; });
-var int32AddMon = monoid2(int32, int32Add, K(0));
-
-var concat = hom(str, str, str)(function (s1, s2) { return s1 + s2; });
-var concat = monoid2(str, concat, K(""));
-
-var mod2 = hom(int32, boolean)(function (n) { return (n % 2) ? true : false; });
-var lsb1 = monoid2(mod2, boolean, xor, K(true));
-var lsb2 = monoid2(int32, mod2, int32Add, K(0));
-// TODO: monoid hom as pullback of the two ways of computing lsb
-
-// MONADS as monoid objects in an endofunctor category
-var monad = function (ftor, times, ident) {    // var monoid = function (set, times, ident) {
-  return function (t) {
-    func(t);                                   
-    return prods({                             //   return prods({
-      t: func,                                 //     t: func,
-      "*": hom(ftor(ftor(t)), ftor(t)),        //     "*": hom(set, set, set),
-      1: hom(t, ftor(t))                       //     1: hom(set)
-    })({                                       //   })({
-      t: ftor(t),                              //     t: set,
-      "*": times(t),                           //     "*": times,
-      1: ident(t)                              //     1: ident
-    });                                        //   });
-  };                                           // };
-}
-
-var lift = function (t) {
-  func(t);
-  return hom(t, arrayOf(t))(function (x) { return [x]; });
-};
-var flatten = function (t) {
-  func(t);
-  return hom(arrayOf(arrayOf(t)), arrayOf(t))(function (llx) {
-    var result = [];
-    var len = llx.length;
-    for (var i = 0; i < len; ++i) {
-      result = result.concat(llx[i]);
-    }
-    return result;
-  });
-};
-
-var listMon = monad(arrayOf, flatten, lift);
-var loi = listMon(int32);
-try {
-  loi.t([3,6,5]); // passes
-  loi.t(["3",6,5]); // fails on item 0
-} catch (e) {}
-loi[1](3); // === [3]
-loi["*"]([[1,2],[3,4],[5]]); // === [1,2,3,4,5]
-
-var upto = hom(int32, arrayOf(int32))(function (x) {
-  var r = [];
-  for (var i = 0; i < x; ++i) {
-    r.push(i);
-  }
-  return r;
-});
-
-loi["*"](listMon(upto).t([6,2])); // === [0,1,2,3,4,5,0,1]
-
-// MONADS in the Haskell / Kleisli style
-var kleisli = function (mon) {
-  return function (t) {
-    var mont = mon(t);
-    function M(mx) {
-      return {
-        value: mx,
-        _: function (f) {
-          return M(mont["*"](mon(f).t(this.value)));
-        }
-      };
-    }
-    return function (x) { return M(mont[1](x)); };
-  };
-};
-
-var kli = kleisli(listMon)(int32);
-kli(6)._(upto)._(upto).value; // === [0,0,1,0,1,2,0,1,2,3,0,1,2,3,4]
-
-// Example of a monoidal closed functor, the "points" functor
-// Comes equipped with natural isomorphisms
-//    chi:(lazy A -o B) -> (lazy A -o lazy B)
-//    phi:(lazy prod_i A_i) -> (prod_i lazy A_i)
-// http://www.cse.chalmers.se/~rjmh/Papers/whyfp.pdf 
-// has lots of examples showing how laziness helps modularization
-
-var lazy = function (c) {
-  // Restrict hom to a single argument,
-  // i.e. [] -> c
-  return hom(c);
-};
-
-var lazyLift = K;
-
-var lazyFlatten = function (lazyLazyX) {
-  return lazyLazyX();
-};
-
-var lazyMon = monad(lazy, lazyLift, lazyFlatten);
-
-var lazyChi = function (lazyAtoB) {
-  return function (lazyA) {
-    return lazyLift(lazyAtoB()(lazyA()));
-  };
-};
-
-var lazyPhi = function (lazyProd) {
-  // Invoke the lazy prod to get the actual prod,
-  // then delay each element.
-  return array(lazyProd()).mapClean(lazyLift);
-};
-
-// Another monoidal closed functor, 
-//    continuation passing is double negation.
-// Dinatural in Z:
-//    cp A = (A->Z)->Z
-var cp = function (c) {
-  return hom(hom(c, id), id);
-};
-
-var cpLift = function (x) {
-  return function (k) {
-    return k(x);
-  };
-};
-
-// Quadruple negation to double negation
-// (((X->Z)->Z)->Z)->Z -> (X->Z)->Z
-var cpFlatten = function (cpCpX /*: (((X->Z)->Z)->Z)->Z */) {
-  return function (k /*: (X->Z) */) {
-    return cpCpX(function (l) {
-      return l(k); /*: Z */
-    });
-  };
-};
-
-var cpMon = monad(cp, cpLift, cpFlatten);
-
-var cpChi = function (k /*: ((A->B)->Z)->Z */) {
-  return function (l /*: (A->Z)->Z */) {
-    return function (m /*: B->Z */) {
-      return l(function (c /*: A */) {
-        return k(function (d /*: A->B */) {
-          return m(d(c));
-        });
-      });
-    };
-  };
-};
-
-// ((prod_i A_i)->Z)->Z -> prod_i (((A_i)->Z)->Z)
-var cpPhi = function (cpProd) {
-  var prod;
-  cpProd(function (p) { prod = array(p); });
-  return p.mapClean(cpLift);
-};
